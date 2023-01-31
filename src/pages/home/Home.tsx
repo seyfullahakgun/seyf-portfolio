941fca5c2f839e283e95e63c34b94d465c46fcf9
Seyfullah Akgun <seyfullahakgun@seyfullahs-mbp.local>
2023-01-31 14:34:38 +0300

    bazi ana ekran geliştirmeleri

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+
+    return (
+        <div className='flex flex-col items-center' id='home-landing'>
+            <div className="w-11/12 mx-auto " style={{ minHeight: '80vh', backgroundImage: 'url(/media/images/seyf-bg.png)', backgroundAttachment: 'initial', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition:'center' }}>
+                <div className='w-full h-full my-auto sticky top-52'>
+                    <div className='w-full h-full flex items-center justify-around'>
+                        <div className=''>aaa</div>
+                        <div className=''>aaa</div>
+                    </div>
+
+                </div>
+            </div>
+            <div className='bg-white w-11/12 h-96 rounded-xl shadow-lg' ></div>‚
+        </div>
+    )
+}
+
+export default Home
